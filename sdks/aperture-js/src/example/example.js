709d063d6da3f9bd9c3d52b59e9e2ae7fab10e66
Hasit Mistry <hasit@fluxninja.com>
2022-10-27 14:37:17 -0700

    Update aperture-js (#829)
    ### Description of change
    
    ##### Checklist
    
    - [ ] Tested in playground or other setup
    - [ ] Documentation is changed or added
    - [ ] Tests and/or benchmarks are included
    - [ ] Breaking changes

--- sdks/aperture-js/src/example/example.js
+++ sdks/aperture-js/src/example/example.js
@@ -1,11 +1,10 @@
-import { client } from "./client.js";
+import { client } from "../client.js";
+import { host, port } from "../consts.js";
 import http from "http";
-import { host, port } from "./consts.js";
-
 
 const requestListener = function (req, res) {
   console.log(`Got a request`);
-  var labelsMap = new Map().set('labelKey','labelValue');
+  var labelsMap = new Map().set('labelKey', 'labelValue');
   client.StartFlow("aperture-js", labelsMap).then(() => {
     console.log('StartFlow Done');
     res.writeHead(200);
@@ -15,5 +14,5 @@ const requestListener = function (req, res) {
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+  console.log(`Server is running on http://${host}:${port}`);
 });
